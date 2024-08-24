d7bb0fdaba04ba0cf0ce02485595b584c498d3f5
Marlon Knupp <marlonjcc23@gmail.com>
2024-08-24 17:04:32 -0300

    ....

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('products/', product_view),
 ]
--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .import models
+from .models import Product
+
+
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title','category', 'brand', 'price',)
+    list_display = ('title','category', 'brand', 'price')
     search_fields = ('title',)
-    list_filter = ('category', 'brand',)
+    list_filter = ('category', 'brand')
 
-admin.site.register(models.Product, ProductAdmin)
+admin.site.register(Product, ProductAdmin)
