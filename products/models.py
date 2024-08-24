48eca9269a4a184750edce0ea117b068f49525b8
Marlon Knupp <marlonjcc23@gmail.com>
2024-08-24 14:16:53 -0300

    updating

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    fprice=models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.title)
+    
