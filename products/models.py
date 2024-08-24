cb3582df8b2a751d65b2fe9d5331de885c91ac67
Marlon Knupp <marlonjcc23@gmail.com>
2024-08-24 15:23:30 -0300

    updating

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    fprice=models.DecimalField(max_digits=10, decimal_places=2)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return str(self.title)
